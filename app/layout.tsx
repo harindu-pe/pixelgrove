4f1d2b61379fd4141c5fcaf4ee05c5b30890b8bb
Harindue <harindu.e@asyncmetric.com>
2024-03-23 23:58:04 +0530

    clerk & modal setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+
+import { ModalProvider } from "@/providers/modal-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
